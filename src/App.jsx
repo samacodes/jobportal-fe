7c371a736f684c4d222e0fbd9810f130edbd76cc
Sama Acharya <asandhya648@gmail.com>
2023-06-25 18:44:46 +0545

    add NavBar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import NavBar from "./Components/NavBar/NavBar";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="w-[85%] m-auto bg-white">
+      <NavBar />
+    </div>
+  );
 };
 
 export default App;
