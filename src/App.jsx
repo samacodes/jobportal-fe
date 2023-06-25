7b62d4aa23396aa2c5c13803e6ccfa8d6fa7753a
Sama Acharya <asandhya648@gmail.com>
2023-06-25 19:17:34 +0545

    add the search field for job search

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import NavBar from "./Components/NavBar/NavBar";
+import Search from "./Components/SearchDiv/Search";
 
 const App = () => {
   return (
     <div className="w-[85%] m-auto bg-white">
       <NavBar />
+      <Search />
     </div>
   );
 };
---
+++ src/Components/SearchDiv/Search.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+
+// Icons
+import { AiOutlineCloseCircle, AiOutlineSearch } from "react-icons/ai";
+import { BsHouseDoor } from "react-icons/bs";
+import { CiLocationOn } from "react-icons/ci";
+
+const Search = () => {
+  return (
+    <div className="searchDiv grid gp-10 bg-greyIsh rounded-[10px] p-[3rem]">
+      <form action="">
+        <div className="firstDiv flex justify-between items-center rounded-[8px] gap-[10px] bg-white p-5 shadow-lg shadow-greyIsh-700">
+          <div className="flex gap-2 items-center">
+            <AiOutlineSearch className="text-[25px] icon" />
+            <input
+              type="text"
+              placeholder="Search for a job..."
+              className="bg-transparent text-blue-500 focus:outline-none w-[100%]"
+            />
+            <AiOutlineCloseCircle className="text-[30px] text-[#a5a6a6] hover:text-textColor icon" />
+          </div>
+
+          <div className="flex gap-2 items-center">
+            <BsHouseDoor className="text-[25px] icon" />
+            <input
+              type="text"
+              placeholder="Search by company..."
+              className="bg-transparent text-blue-500 focus:outline-none w-[100%]"
+            />
+            <AiOutlineCloseCircle className="text-[30px] text-[#a5a6a6] hover:text-textColor icon" />
+          </div>
+
+          <div className="flex gap-2 items-center">
+            <CiLocationOn className="text-[25px] icon" />
+            <input
+              type="text"
+              placeholder="Search by location..."
+              className="bg-transparent text-blue-500 focus:outline-none w-[100%]"
+            />
+            <AiOutlineCloseCircle className="text-[30px] text-[#a5a6a6] hover:text-textColor icon" />
+          </div>
+          <button className="bg-blueColor h-full p-5 px-10 rounded-[10px] text-white cursor-pointer hover:bg-blue-400">
+            Search
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Search;
