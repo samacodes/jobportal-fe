b5eeb20197e637ab049ba5af56ac12f1a3dedd9a
Sama Acharya <asandhya648@gmail.com>
2023-07-18 09:25:00 +0545

    add user registration page

--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -1,7 +1,110 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import "../App.css";
+import api from "../Helpers/AxiosHelper";
 
 const Register = () => {
-  return <div>Register</div>;
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const [formValue, setFormValue] = useState({});
+  const navigate = useNavigate();
+
+  const handleUsernameChange = (e) => {
+    setUsername(e.target.value);
+    setError("");
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+    setError("");
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const newUser = {
+      username: username,
+      password: password,
+    };
+
+    api
+      .post("/auth/register", newUser)
+      .then((res) => {
+        console.log(res.data);
+        navigate("/login");
+      })
+      .catch((err) => {
+        console.log(err);
+        setError("Registration failed");
+      });
+  };
+
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <div className="logoDiv mb-8">
+        <h1 className="logo text-3xl text-blue-500">
+          <strong>Job</strong>Portal
+        </h1>
+      </div>
+      <form
+        className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 max-w-sm w-full"
+        onSubmit={handleSubmit}
+      >
+        {/* Username input field */}
+        <div className="mb-4">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="username"
+          >
+            Username
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="username"
+            type="text"
+            placeholder="Username"
+            value={username}
+            onChange={handleUsernameChange}
+          />
+        </div>
+        {/* Password input field */}
+        <div className="mb-6">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="password"
+          >
+            Password
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+            id="password"
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={handlePasswordChange}
+          />
+          {/* Error message */}
+          {error && <p className="text-red-500 text-sm mb-3">{error}</p>}
+        </div>
+        {/* Submit button */}
+        <div className="flex flex-col items-center justify-center">
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mb-2 w-full"
+            type="submit"
+          >
+            Register
+          </button>
+          <p className="text-gray-600 text-sm">
+            Already have an account?{" "}
+            <Link to="/login" className="text-blue-500">
+              Sign in
+            </Link>
+          </p>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default Register;
