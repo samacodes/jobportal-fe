ff9c216e090ee0447528c4a20ede602525019cdf
Sama Acharya <asandhya648@gmail.com>
2023-07-18 11:44:32 +0545

    change navbar to only show login / logout options

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,28 +1,41 @@
 import React from "react";
 
+const LoginButt = () => {
+  return (
+    <>
+      <li className="menuList text-[#6f6f6f] hover:text-blueColor">
+        <a href="/login">Log In</a>
+      </li>
+    </>
+  );
+};
+
+const LogoutButt = () => {
+  const logout = () => {
+    localStorage.removeItem("token");
+  };
+  return (
+    <>
+      <li className="menuList text-[#6f6f6f] hover:text-blueColor">
+        <a href="/" onClick={logout}>
+          Log Out
+        </a>
+      </li>
+    </>
+  );
+};
+
 const NavBar = () => {
   return (
     <div className="navBar flex justify-between items-center p-[3rem]">
       <div className="logoDiv">
         <h1 className="logo text-[25px] text-blueColor">
-          <strong>Job</strong>Portal
+          <strong>Job</strong>Portal{" "}
         </h1>
       </div>
 
       <div className="menu flex gap-8">
-        <li className="menuList text-[#6f6f6f] hover:text-blueColor">Jobs</li>
-        <li className="menuList text-[#6f6f6f] hover:text-blueColor">
-          Companies
-        </li>
-        <li className="menuList text-[#6f6f6f] hover:text-blueColor">About</li>
-        <li className="menuList text-[#6f6f6f] hover:text-blueColor">
-          Contact
-        </li>
-        <li className="menuList text-[#6f6f6f] hover:text-blueColor">Blog</li>
-        <li className="menuList text-[#6f6f6f] hover:text-blueColor">Login</li>
-        <li className="menuList text-[#6f6f6f] hover:text-blueColor">
-          Register
-        </li>
+        {localStorage.getItem("token") ? <LogoutButt /> : <LoginButt />}
       </div>
     </div>
   );
