587411783d704483e99871e4f3f97e21a59ea6b0
Sama Acharya <asandhya648@gmail.com>
2023-07-24 21:12:38 +0545

    change the search component ui

--- src/Components/SearchDiv/Search.jsx
+++ src/Components/SearchDiv/Search.jsx
@@ -1,101 +1,140 @@
-import React from "react";
-
-// Icons
+import React, { useState } from "react";
 import { AiOutlineCloseCircle, AiOutlineSearch } from "react-icons/ai";
 import { BsHouseDoor } from "react-icons/bs";
-import { CiLocationOn } from "react-icons/ci";
+import { useNavigate } from "react-router-dom";
 
 const Search = () => {
+  const [jobQuery, setJobQuery] = useState("");
+  const [locationQuery, setLocationQuery] = useState("");
+  const [showSorting, setShowSorting] = useState(false);
+  const [jobFocus, setJobFocus] = useState(false);
+  const [locationFocus, setLocationFocus] = useState(false);
+  const navigate = useNavigate();
+
+  const handleSearch = (event) => {
+    event.preventDefault();
+    setShowSorting(true);
+    // Redirect to the search page with filters using useNavigate
+    navigate(
+      `/search?job=${encodeURIComponent(
+        jobQuery
+      )}&location=${encodeURIComponent(locationQuery)}`
+    );
+  };
+
+  const handleClearAll = () => {
+    setJobQuery("");
+    setLocationQuery("");
+    setShowSorting(false);
+  };
+
   return (
-    <div className="searchDiv grid gap-10 bg-greyIsh rounded-[10px] p-[3rem]">
-      <form action="">
-        <div className="firstDiv flex justify-between items-center rounded-[8px] gap-[10px] bg-white p-5 shadow-lg shadow-greyIsh-700">
-          <div className="flex gap-2 items-center">
-            <AiOutlineSearch className="text-[25px] icon" />
-            <input
-              type="text"
-              placeholder="Search for a job..."
-              className="bg-transparent text-blue-500 focus:outline-none w-[100%]"
+    <div className="searchDiv p-[3rem] flex justify-center">
+      <form
+        onSubmit={handleSearch}
+        className="flex flex-wrap gap-4 justify-center"
+      >
+        {/* Search by Job */}
+        <div
+          className={`flex items-center gap-2 border rounded-lg p-2 border-[#555555] text-black text-lg w-[400px] ${
+            jobFocus ? "focused" : ""
+          }`}
+          onFocus={() => setJobFocus(true)}
+          onBlur={() => setJobFocus(false)}
+        >
+          <AiOutlineSearch className="text-[20px] text-blue-500" />
+          <input
+            type="text"
+            placeholder="Search for a job..."
+            value={jobQuery}
+            onChange={(e) => setJobQuery(e.target.value)}
+            className="bg-transparent text-black focus:outline-none w-full"
+          />
+          {jobQuery && (
+            <AiOutlineCloseCircle
+              className="text-[20px] text-[#a5a6a6] hover:text-textColor cursor-pointer"
+              onClick={() => setJobQuery("")}
             />
-            <AiOutlineCloseCircle className="text-[30px] text-[#a5a6a6] hover:text-textColor icon" />
-          </div>
-
-          <div className="flex gap-2 items-center">
-            <BsHouseDoor className="text-[25px] icon" />
-            <input
-              type="text"
-              placeholder="Search by company..."
-              className="bg-transparent text-blue-500 focus:outline-none w-[100%]"
-            />
-            <AiOutlineCloseCircle className="text-[30px] text-[#a5a6a6] hover:text-textColor icon" />
-          </div>
-
-          <div className="flex gap-2 items-center">
-            <CiLocationOn className="text-[25px] icon" />
-            <input
-              type="text"
-              placeholder="Search by location..."
-              className="bg-transparent text-blue-500 focus:outline-none w-[100%]"
-            />
-            <AiOutlineCloseCircle className="text-[30px] text-[#a5a6a6] hover:text-textColor icon" />
-          </div>
-          <button className="bg-blueColor h-full p-5 px-10 rounded-[10px] text-white cursor-pointer hover:bg-blue-400">
-            Search
-          </button>
+          )}
         </div>
+
+        {/* Search by Location */}
+        <div
+          className={`flex items-center gap-2 border rounded-lg p-2 border-[#555555] text-black text-lg w-[400px] ${
+            locationFocus ? "focused" : ""
+          }`}
+          onFocus={() => setLocationFocus(true)}
+          onBlur={() => setLocationFocus(false)}
+        >
+          <BsHouseDoor className="text-[20px] text-blue-500" />
+          <input
+            type="text"
+            placeholder="Search by location..."
+            value={locationQuery}
+            onChange={(e) => setLocationQuery(e.target.value)}
+            className="bg-transparent text-black focus:outline-none w-full"
+          />
+          {locationQuery && (
+            <AiOutlineCloseCircle
+              className="text-[20px] text-[#a5a6a6] hover:text-textColor cursor-pointer"
+              onClick={() => setLocationQuery("")}
+            />
+          )}
+        </div>
+
+        {/* Search Button */}
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mb-2 w-[400px]">
+          Search
+        </button>
       </form>
 
-      <div className="secDiv flex items-center gap-10 justify-center">
-        <div className="singleSearch flex items-center gap-2">
-          <label htmlFor="relevance" className="text-[#808080] font-semibold">
-            Sort by:
-          </label>
-          <select
-            name=""
-            id="relevance"
-            className="bg-white rounded-[3px] px-4 py-1"
+      {showSorting && (
+        <>
+          {/* Clear All */}
+          <span
+            className="mt-4 text-[#a1a1a1] cursor-pointer text-sm"
+            onClick={handleClearAll}
           >
-            <option value="">Relevance</option>
-            <option value="">Inclusive</option>
-            <option value="">Starts with</option>
-            <option value="">Contains</option>
-          </select>
-        </div>
+            Clear All
+          </span>
 
-        <div className="singleSearch flex items-center gap-2">
-          <label htmlFor="type" className="text-[#808080] font-semibold">
-            Type:
-          </label>
-          <select
-            name=""
-            id="type"
-            className="bg-white rounded-[3px] px-4 py-1"
-          >
-            <option value="">Full-time</option>
-            <option value="">Remote</option>
-            <option value="">Contract</option>
-            <option value="">Part-time</option>
-          </select>
-        </div>
+          {/* Sorting Options */}
+          <div className="mt-4 flex flex-wrap items-center gap-4">
+            <div className="flex items-center gap-2 text-sm">
+              <label
+                htmlFor="sortType"
+                className="text-[#808080] font-semibold"
+              >
+                Sort by:
+              </label>
+              <select
+                id="sortType"
+                className="bg-white rounded-[3px] px-3 py-1 text-sm"
+              >
+                <option value="">Relevance</option>
+                <option value="">Date Posted</option>
+                <option value="">Salary</option>
+              </select>
+            </div>
 
-        <div className="singleSearch flex items-center gap-2">
-          <label htmlFor="level" className="text-[#808080] font-semibold">
-            Level:
-          </label>
-          <select
-            name=""
-            id="level"
-            className="bg-white rounded-[3px] px-4 py-1"
-          >
-            <option value="">Senior</option>
-            <option value="">Beginner</option>
-            <option value="">Intermediate</option>
-            <option value="">Advocate</option>
-          </select>
-        </div>
-
-        <span className="text-[#a1a1a1] cursor-pointer">Clear All</span>
-      </div>
+            <div className="flex items-center gap-2 text-sm">
+              <label
+                htmlFor="sortOrder"
+                className="text-[#808080] font-semibold"
+              >
+                Order:
+              </label>
+              <select
+                id="sortOrder"
+                className="bg-white rounded-[3px] px-3 py-1 text-sm"
+              >
+                <option value="asc">Ascending</option>
+                <option value="desc">Descending</option>
+              </select>
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 };
